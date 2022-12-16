da567aecda646317298a781571f689e43e234042
Erik Opinaldo <erikopinaldo@gmail.com>
2022-12-16 16:59:34 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import logger from "morgan";
+import cors from "cors";
+
+// routes
+import indexRouter from "./routes/index.js";
+import userRouter from "./routes/user.js";
+import chatRoomRouter from "./routes/chatRoom.js";
+import deleteRouter from "./routes/delete.js";
+
+// middleware
+import { decode } from './middlewares/jwt.js'
+
+const port = process.env.PORT || '8000'
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+app.use("/users", userRouter);
+app.use("/room", decode, chatRoomRouter);
+app.use("/delete", deleteRouter);
+
+/** catch 404 and forward to error handler */
+app.use('*', (req, res) => {
+    return res.status(404).json({
+        success: false,
+        message: 'API endpoint doesnt exist'
+    })
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is running, you better catch it!')
+}) 
