542eb8b1a667b8a3e89ffa132c8231846833fecc
Erik Opinaldo <erikopinaldo@gmail.com>
2022-12-16 17:04:06 -0500

    added index route skeleton

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,14 @@
+import express from 'express';
 
+// controllers
+import users from '../controllers/user.js';
+
+// middlewares
+import { encode } from '../middlewares/jwt.js';
+
+const router = express.Router();
+
+router
+    .post('/login/:userId', encode, (req, res, next) => { });
+
+export default router;
