35b89981f49586d3cfc993b0411c85720fd4c976
Erik Opinaldo <erikopinaldo@gmail.com>
2022-12-18 18:13:29 -0500

    add onDeleteUserById method

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -40,5 +40,15 @@ export default {
             return res.status(500).json({ success: false, error: error })
         }
     },
-    onDeleteUserById: async (req, res) => { },
+    onDeleteUserById: async (req, res) => {
+        try {
+            const user = await UserModel.deleteByUserById(req.params.id);
+            return res.status(200).json({
+                success: true,
+                message: `Deleted a count of ${user.deletedCount} user.`
+            });
+        } catch (error) {
+            return res.status(500).json({ success: false, error: error })
+        }
+    },
 }
