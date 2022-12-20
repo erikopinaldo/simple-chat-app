cf6eef8d052e1ad671b8852bcf13c4381c3053e6
Erik Opinaldo <erikopinaldo@gmail.com>
2022-12-19 19:35:03 -0500

    store app.listen() return value in server variable

--- server/index.js
+++ server/index.js
@@ -42,6 +42,6 @@ app.use('*', (req, res) => {
     })
 });
 
-app.listen(process.env.PORT, () => {
+let server = app.listen(process.env.PORT, () => {
     console.log('Server is running, you better catch it!')
 }) 
